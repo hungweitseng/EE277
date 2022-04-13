87b1552d5944c816a819b29d8257aec54125dbca
hunweitseng <h1tseng@cs.ucsd.edu>
2022-04-13 15:01:40 -0700

    Updated memory bandwidth profiler

--- demo/memory/memory_profile.c
+++ demo/memory/memory_profile.c
@@ -23,6 +23,7 @@ void write_memory_avx(void* array, size_t size) {
   size_t i;
   for (i = 0; i < size / sizeof(__m256i); i++) {
     _mm256_store_si256(&varray[i], vals);  // This will generate the vmovaps instruction.
+
   }
 }
 #endif
@@ -30,12 +31,14 @@ void write_memory_avx(void* array, size_t size) {
 int main(int argc, char **argv)
 {
     size_t *array;
-    size_t size;
+    size_t size, number_of_elements;
+    size_t sum;
     double total_time;
     struct timespec start, end;
     int i;
-    size = atoi(argv[1])/sizeof(size_t);
-    array = (size_t *)memalign(32,sizeof(size_t)*size);
+    size = atoi(argv[1]);
+    number_of_elements = size/sizeof(size_t);
+    array = (size_t *)memalign(32,size);
     clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
     #ifdef SIMD
     write_memory_avx(array, size);
@@ -44,16 +47,29 @@ int main(int argc, char **argv)
     #endif
     clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
     total_time = ((end.tv_sec * 1000000000.0 + end.tv_nsec) - (start.tv_sec * 1000000000.0 + start.tv_nsec));
-    fprintf(stderr,"Latency: %.0lf ns, GBps: %lf\n",total_time, (double)((double)size*sizeof(size_t)/(total_time)));
-    clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+    #ifdef CSV
+    fprintf(stderr,"%.0lf,\t",total_time);
+    #else
+    fprintf(stderr,"Latency: %.0lf ns, GBps: %lf\n",total_time, (double)((double)size/(total_time)));
+    #endif
+    total_time = 0.0;
     for(i = 0 ; i< 10; i++)
+    {
+    clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
     #ifdef SIMD
     write_memory_avx(array, size);
     #else
     write_memory_loop(array, size);
     #endif
     clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
-    total_time = ((end.tv_sec * 1000000000.0 + end.tv_nsec) - (start.tv_sec * 1000000000.0 + start.tv_nsec));
-    fprintf(stderr,"Latency (10x average): %.0lf ns, GBps (10x average): %lf\n",total_time/10, (double)((double)size*10*sizeof(size_t)/(total_time)));
+    total_time += ((end.tv_sec * 1000000000.0 + end.tv_nsec) - (start.tv_sec * 1000000000.0 + start.tv_nsec));
+    sum += array[rand()%number_of_elements];
+    }
+#ifdef CSV
+    fprintf(stderr,"%.0lf,\t",total_time/10);
+#else
+    fprintf(stderr,"Latency (10x average): %.0lf ns, GBps (10x average): %lf\n",total_time/10, (double)((double)size*10/(total_time)));
+#endif
+
     return 0;
 }
